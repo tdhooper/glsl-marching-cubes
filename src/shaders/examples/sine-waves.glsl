b717024b137a88c94287e9ae28f396207c903a83
Thomas Hooper <tom@stainlessvision.com>
2016-02-20 20:36:37 +0000

    Scale sine waves example

--- src/shaders/examples/sine-waves.glsl
+++ src/shaders/examples/sine-waves.glsl
@@ -1,9 +1,10 @@
 
 // BOUNDING_BOX_POSITION 0 0 0
-// BOUNDING_BOX_SIZE 10 10 10
+// BOUNDING_BOX_SIZE 3 3 3
 
 // Your glsl signed distance function:
 
 float mapDistance(vec3 p) {
+  p *= 5.;
   return sin(p.x) + sin(p.y) + sin(p.z);
 }
